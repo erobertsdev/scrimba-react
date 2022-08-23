aa1892e2a9eaaff67549fc980c5f89256b563fd8
Elias Roberts <elias@eroberts.dev>
2022-08-23 15:14:01 -0600

    declarative example

--- hello-react/index.js
+++ hello-react/index.js
@@ -1,7 +1,9 @@
 // Imperative example
-const root = document.getElementById('root');
-const newEl = document.createElement('h1');
-newEl.textContent = "This is some text content";
-newEl.className = 'header';
-root.appendChild(newEl);
+// const root = document.getElementById('root');
+// const newEl = document.createElement('h1');
+// newEl.textContent = "This is some text content";
+// newEl.className = 'header';
+// root.appendChild(newEl);
+
+ReactDOM.render(<h1 className='header'>This is some text content</h1>, document.getElementById('root'));
 
