834b85652745456e17f3327e7e6885525dad43fd
Elias Roberts <elias@eroberts.dev>
2022-08-24 13:22:45 -0600

    quick nav challenge

--- hello-react/index.js
+++ hello-react/index.js
@@ -7,11 +7,15 @@
 
 // JSX can be stored in a variable. wtf
 
-const page = (
-	<div>
-		<h1 className='header'>This is SPARTA</h1>
-		<p>This is a paragraph</p>
-	</div>
+const navbar = (
+	<nav>
+		<h1>Oliver Inc.</h1>
+		<ul>
+			<li>Pricing</li>
+			<li>About</li>
+			<li>Contact</li>
+		</ul>
+	</nav>
 );
 
-ReactDOM.render(page, document.getElementById('root'));
+ReactDOM.render(navbar, document.getElementById('root'));
