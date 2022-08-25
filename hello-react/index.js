6039f2717560ee40f9ca19498a3deea16e01e684
Elias Roberts <elias@eroberts.dev>
2022-08-25 17:38:56 -0600

    first functional component wow

--- hello-react/index.js
+++ hello-react/index.js
@@ -7,15 +7,24 @@
 
 // JSX can be stored in a variable. wtf
 
-const navbar = (
-	<nav>
-		<h1>Oliver Inc.</h1>
-		<ul>
-			<li>Pricing</li>
-			<li>About</li>
-			<li>Contact</li>
-		</ul>
-	</nav>
-);
 
-ReactDOM.render(navbar, document.getElementById('root'));
+const Navbar = () => {
+	return (
+		<nav>
+			<h1>Oliver Inc.</h1>
+			<ul>
+				<li>Pricing</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	);
+}
+
+// ReactDOM.render(navbar, document.getElementById('root'));
+// React 18 version
+ReactDOM.createRoot(document.getElementById('root')).render(<Navbar />);
+
+// React 18 common version
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Navbar />);
